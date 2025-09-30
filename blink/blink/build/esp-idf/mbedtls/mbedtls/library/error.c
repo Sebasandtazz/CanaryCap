9f337c6b499a0152a8d9a9757962a553a66f7ae5
Axlrod223 <72531783+Axlrod223@users.noreply.github.com>
2025-09-30 16:28:13 -0400

    no blink

--- blink/blink/build/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/Users/justinbetz/Documents/GitHub/CanaryCap/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
--- blink/blink/build/esp-idf/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
+++
@@ -1,1 +0,0 @@
-/Users/justinbetz/Documents/GitHub/CanaryCap/esp/esp-idf/components/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
